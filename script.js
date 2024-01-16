2be4b580c0a75b885958f6979632796a6a4d396f
Your Name <you@example.com>
2024-01-16 10:51:29 -0500

    3

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var AllValues = [];
+var doubles = 0;
+var triplets = 0;
+var sums = [];
 
 function getInfo(form) {
     var numDice = (form.numDice.value);
@@ -6,11 +9,14 @@ function getInfo(form) {
     if (numTimes == "") {
         numTimes = 1;
     }
-    console.log(numDice + " and " + numTimes);
     AllValues = [];
+    doubles = 0;
+    triplets = 0;
+    rollDice(parseInt(numDice), parseInt(numTimes));
 }
 
 function rollDice(numDice,numTimes) {
+    sums = [];
     for (let i = 0; i < numTimes; i++) {
         var dice1 = 0;
         var dice2 = 0;
@@ -18,60 +24,125 @@ function rollDice(numDice,numTimes) {
         switch (numDice){
             case 3:
             dice3=random();
+            dice2=random();
+            dice1=random();
+            if (dice1==dice2 && dice3 == dice2) {
+                triplets++;
+            }
             case 2:
             dice2=random();
+            dice1=random();
+            if(dice1 == dice2) {
+                doubles++;
+            }
             case 1:
             dice1=random();
+            break;
         }
         if (dice1 != 0) {
             AllValues.push(dice1);
-        } else if (dice2 != 0) {
+        } if (dice2 != 0) {
             AllValues.push(dice2);
-        } else if (dice3 != 0) {
+        } if (dice3 != 0) {
             AllValues.push(dice3);
         }
+        sums.push(dice1+dice2+dice3);
     }
+    
     console.log(AllValues);
+    data(numDice);
 }
 
-function data() {
-    AllValues = AllValues.sort();
-    const sum = AllValues.reduce(
+function data(numDice) {
+    sums = sums.sort();
+    const sum = sums.reduce(
         (accumulator, currentValue) => accumulator + currentValue
       );
-    console.log(sum);
 
-    const mean = sum/AllValues.length;
-    console.log (mean.toFixed(3));
+    const mean = sum/sums.length;
+    console.log (mean.toFixed(2));
 
     var median = 0;
-    if (AllValues.length % 2 == 0) {
-        var front = (AllValues.length-1)/2 +.5;
-        var back = (AllValues.length-1)/2 -.5;
-        median = (AllValues[front] + AllValues[back])/2
+    if (sums.length % 2 == 0) {
+        var front = (sums.length)/2;
+        var back = (sums.length)/2-1;
+        median = (sums[front] + sums[back])/2
     } else {
-        median = AllValues[(AllValues.length-1)/2];
+        median = sums[(sums.length-1)/2];
     }
     console.log(median);
 
-    let occur = 0;
-    let value = AllValues[0];
-    maxOccur = 0;
-    for (let i = 0; i < AllValues.length; i++) {
-        if (AllValues[i] == value) {
-            occur++;
-        } else {
-            value = AllValues[i];
-            if (occur > maxOccur) {
-                maxOccur = occur;
-            }
-            occur = 0;
+    let occurences = count(sums);
+    var mode = [];
+    var max = 0
+    for (let i = 1; i <=18; i++) {
+        if (occurences[i] > max) {
+            max = occurences[i];
         }
     }
-    console.log(maxOccur)
+    for (let i = 1; i <=18; i++) {
+        if (occurences[i] == max) {
+            mode.push(i);
+        }
+    }
+    console.log(mode);
+
+    let frequencies = count(sums);
+    console.log(frequencies);
+    update(frequencies, mean, median, mode, numDice);
 }
 
+//https://stackoverflow.com/questions/5667888/counting-the-occurrences-frequency-of-array-elements 
+function count(arr) {
+    return arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {})
+}
+  
 function random() {
     return Math.floor(Math.random() * (6) + 1)
+}
+
+function update(frequencies, mean, median, mode, numDice) {
+    console.log(frequencies[1]);
+    const table = document.querySelector('table');
+    const tblBody = document.createElement('tbody');
+    var cellText = '';
+    const row = document.createElement('tr');
+
+    for (let i = 0; i < 3; i++) {
+        const cell = document.createElement('td');
+        switch (i) {
+            case 0:
+                outcomes = 0;
+                if (numDice == 1) {
+                    outcomes = 6;
+                } else if (numDice == 2) {
+                    outcomes = 12;
+                } else if (numDice == 3) {
+                    outcomes = 18
+                } 
+                var text = ``;
+                for (let i = 1; i <=outcomes; i++ ) {
+                    if (frequencies[i] == undefined) {
+                        frequencies[i] = 0;
+                    }
+                    text+= `${i}:${frequencies[i]} `
+                }
+                cellText = document.createTextNode(text);
+                break;
+            case 1: 
+                cellText = document.createTextNode(`Mean: ${mean}\nMedian: ${median}\nMode: ${mode}`);
+                break;
+            case 2:
+                cellText = document.createTextNode(`Doubles: ${doubles}\nTriplets: ${triplets}`);
+                break;
+        }
+
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+    }
+    tblBody.appendChild(row);
+    table.appendChild(tblBody);
+
+
 }
 
